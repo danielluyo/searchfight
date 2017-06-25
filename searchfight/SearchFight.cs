07f354df55447b7269d4b0a0e2f489e0e3c755db
danielluyo <danielluyo@gmail.com>
2017-06-24 20:27:56 -0500

    Init version

---
+++ searchfight/SearchFight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchfight
+{
+    class SearchFight
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">The programs requires a list of search terms. If no arguments are provided a
+        ///                    help text is provided.</param>
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ShowUsage();
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Show help for the program.
+        /// </summary>
+        static private void ShowUsage()
+        {
+            Console.WriteLine("Usage: searchfight [search_term...] | [-?]");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("    -?            Shows this help.");            
+        }
+    }
+}
